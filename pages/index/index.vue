e77d14ceb08bd019394c7d5c5047cd37b40ebb85
wen <756307849@qq.com>
2020-12-09 00:58:01 +0800

    增加双12活动

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -36,26 +36,26 @@ export default {
 					text: '饿了么',
 					tabId: 1,
 				},
+				// {
+				// 	icon: '/static/meituan.png',
+				// 	text: '美团',
+				// 	tabId: 2,
+				// },
+				// {
+				// 	icon: '/static/jd.png',
+				// 	text: '京东',
+				// 	tabId: 3,
+				// },
+				// {
+				// 	icon: '/static/vip.png',
+				// 	text: 'VIP会员',
+				// 	tabId: 4,
+				// },
 				{
-					icon: '/static/meituan.png',
-					text: '美团',
-					tabId: 2,
-				},
-				{
-					icon: '/static/11.png',
-					text: '双十一',
+					icon: '/static/12.png',
+					text: '双十二',
 					tabId: 5,
 				},
-				{
-					icon: '/static/jd.png',
-					text: '京东',
-					tabId: 3,
-				},
-				{
-					icon: '/static/vip.png',
-					text: 'VIP会员',
-					tabId: 4,
-				}
 			],
 			couponList: [],
 			coupons: [
@@ -63,61 +63,116 @@ export default {
 					name: '饿了么红包',
 					icon: '/static/coupon/ele.png',
 					bannerPic: '/static/coupon/ele_banner.png',
-					url: 'https://s.click.ele.me/frZOjvu',
+					url: 'https://s.click.ele.me/S4UMXtu',
 					type: 1,
 					tabId: 1,
 					minapp: {
 						appid: 'wxece3a9a4c82f58c9',
-						path: 'pages/sharePid/web/index?scene=https://s.click.ele.me/wR9ecuu'
+						path: 'pages/sharePid/web/index?scene=https://s.click.ele.me/S4UMXtu'
 					}
 				},
-				{
-					name: '美团外卖红包',
-					icon: '/static/coupon/meituan.png',
-					bannerPic: '/static/coupon/meituan_banner.png',
-					url:'https://runion.meituan.com/url?key=cd23768d09c339d1641b2738df39aa67&url=https%3A%2F%2Fi.meituan.com%2Fawp%2Fhfe%2Fblock%2Fa945391288b790d558b7%2F78716%2Findex.html%3Fappkey%3Dcd23768d09c339d1641b2738df39aa67%3Ajuhe&sid=juhe',
-					type: 1,
-					tabId: 2,
-					minapp: {
-						appid: 'wxde8ac0a21135c07d',
-						path: '/index/pages/h5/h5?weburl=https%3A%2F%2Frunion.meituan.com%2Furl%3Fkey%3D591ec05930c57331c1212b936e6785c1%26url%3Dhttps%253A%252F%252Fi.meituan.com%252Fawp%252Fhfe%252Fblock%252Fa13b87919a9ace9cfab4%252F89400%252Findex.html%253Fappkey%253D591ec05930c57331c1212b936e6785c1%253A000001%26sid%3D000001&lch=cps:waimai:5:591ec05930c57331c1212b936e6785c1:000001&f_token=1&f_userId=1'
-					}
-				},
-				{
-					name: '爱奇艺会员',
-					icon: '/static/coupon/vip.png',
-					bannerPic: '/static/coupon/vip_banner.png',
-					url:'https://p.pinduoduo.com/VJ7bHo5d',
-					type: 2,
-					tabId: 4
-				},
-				{
-					name: '三只松鼠大礼包',
-					icon: '/static/coupon/jd.png',
-					bannerPic: '/static/coupon/sanzhisongshu.png',
-					url:'https://u.jd.com/tFDejq',
-					type: 0,
-					tabId: 3
-				},
+				// {
+				// 	name: '美团外卖红包',
+				// 	icon: '/static/coupon/meituan.png',
+				// 	bannerPic: '/static/coupon/meituan_banner.png',
+				// 	url:'https://runion.meituan.com/url?key=cd23768d09c339d1641b2738df39aa67&url=https%3A%2F%2Fi.meituan.com%2Fawp%2Fhfe%2Fblock%2Fa945391288b790d558b7%2F78716%2Findex.html%3Fappkey%3Dcd23768d09c339d1641b2738df39aa67%3Ajuhe&sid=juhe',
+				// 	type: 1,
+				// 	tabId: 2,
+				// 	minapp: {
+				// 		appid: 'wxde8ac0a21135c07d',
+				// 		path: '/index/pages/h5/h5?weburl=https%3A%2F%2Frunion.meituan.com%2Furl%3Fkey%3D591ec05930c57331c1212b936e6785c1%26url%3Dhttps%253A%252F%252Fi.meituan.com%252Fawp%252Fhfe%252Fblock%252Fa13b87919a9ace9cfab4%252F89400%252Findex.html%253Fappkey%253D591ec05930c57331c1212b936e6785c1%253A000001%26sid%3D000001&lch=cps:waimai:5:591ec05930c57331c1212b936e6785c1:000001&f_token=1&f_userId=1'
+				// 	}
+				// },
+				// {
+				// 	name: '爱奇艺会员',
+				// 	icon: '/static/coupon/vip.png',
+				// 	bannerPic: '/static/coupon/vip_banner.png',
+				// 	url:'https://p.pinduoduo.com/VJ7bHo5d',
+				// 	type: 2,
+				// 	tabId: 4
+				// },
+				// {
+				// 	name: '三只松鼠大礼包',
+				// 	icon: '/static/coupon/jd.png',
+				// 	bannerPic: '/static/coupon/sanzhisongshu.png',
+				// 	url:'https://u.jd.com/tFDejq',
+				// 	type: 0,
+				// 	tabId: 3
+				// },
 				{
 					name: '饿了么果蔬',
 					icon: '/static/coupon/ele.png',
 					bannerPic: '/static/coupon/ele_guosu.png',
-					url:'https://s.click.ele.me/RpRFhvu',
+					url:'https://s.click.ele.me/ibgcTtu',
 					type: 1,
 					tabId: 1,
 					minapp: {
 						appid: 'wxece3a9a4c82f58c9',
-						path: 'pages/sharePid/web/index?scene=https://s.click.ele.me/I4Yacuu'
+						path: 'pages/sharePid/web/index?scene=https://s.click.ele.me/ibgcTtu'
 					}
 				},
 				{
-					name: '抽红包立减',
-					icon: '/static/11.png',
-					bannerPic: '/static/coupon/11.jpg',
-					url:'https://s.click.taobao.com/Gcs9vuu',
-					type: 2,
-					tabId: 5
+					name: '淘宝双12—主会场',
+					icon: '/static/12.png',
+					bannerPic: '/static/coupon/M1IIWtu.jpg',
+					url:'https://s.click.taobao.com/M1IIWtu',
+					type: 0,
+					tabId: 5,
+					copy: '7👈￥1nT7cL8Fx1O￥回👉🍑寳👈或點缶链节 https://m.tb.cn/h.45OVJVm 至浏览er【2020淘宝双12—超级红包主会场(每满200减25，更有最高1212红包等你来抢~）】',
+				},
+				{
+					name: '天猫双12—主会场',
+					icon: '/static/12.png',
+					bannerPic: '/static/coupon/4qiHWtu.jpg',
+					url:'https://s.click.taobao.com/4qiHWtu',
+					type: 0,
+					tabId: 5,
+					copy: '4👈￥eMTtcLj3unV￥回👉🍑寳👈或點缶链节 https://m.tb.cn/h.45Ompu1 至瑬览器【2020天喵双12—主会场】'
+				},
+				{
+					name: '品牌快抢',
+					icon: '/static/12.png',
+					bannerPic: '/static/coupon/5kTjStu.jpg',
+					url:'https://s.click.taobao.com/5kTjStu',
+					type: 0,
+					tabId: 5,
+					copy: '1👈￥iEekcLj3p4D￥噠閞℡👉t~bao👈或點̸击̸url链 https://m.tb.cn/h.45OmNru 至瑬览器【品牌快抢，大牌放价天天有3折】'
+				},
+				{
+					name: '瓜分千亿淘金币',
+					icon: '/static/12.png',
+					bannerPic: '/static/coupon/T6MHWtu.png',
+					url:'https://s.click.taobao.com/T6MHWtu',
+					type: 0,
+					tabId: 5,
+					copy: '3👈￥7QDjcLj3jaz￥回👉tb👈或點҉击҉链节 https://m.tb.cn/h.4fgFUwR 至瑬览器【淘宝双12-互动玩法，瓜分千亿淘金币】'
+				},
+				{
+					name: '聚划算年终盛典',
+					icon: '/static/12.png',
+					bannerPic: '/static/coupon/kfBHWtu.jpg',
+					url:'https://s.click.taobao.com/kfBHWtu',
+					type: 0,
+					tabId: 5,
+					copy: '6.0￥7imZcLj3n2x￥回👉🍑寳👈或點̸击̸url链 https://m.tb.cn/h.4U0kfPM 至瑬览器【12.12聚划算年终盛典】'
+				},
+				{
+					name: '双12全球尖货',
+					icon: '/static/12.png',
+					bannerPic: '/static/coupon/oVhGWtu.jpg',
+					url:'https://s.click.taobao.com/oVhGWtu',
+					type: 0,
+					tabId: 5,
+					copy: '7👈￥q9V2cLj3YHd￥噠閞℡👉t~bao👈或點几url链 https://m.tb.cn/h.4fgwcv4 至浏览er【双12全球尖货】'
+				},
+				{
+					name: '双12官方直营',
+					icon: '/static/12.png',
+					bannerPic: '/static/coupon/PECGWtu.jpg',
+					url:'https://s.click.taobao.com/PECGWtu',
+					type: 0,
+					tabId: 5,
+					copy: '7👈￥hz10cLjeVxb￥回👉🍑寳👈或點҉击҉链节 https://m.tb.cn/h.45OmNyP 至浏览er【天喵国际直营双十二现货满600减300】'
 				},
 			]
 		};
@@ -186,6 +241,25 @@ export default {
 				  success(res) {
 					// 打开成功
 				  }
+				})
+			}
+			if(this.couponList[i].copy){
+				// 设置剪切板内容
+				wx.setClipboardData({
+				    data: this.couponList[i].copy,
+				    success (res) {
+						wx.getClipboardData({
+						    success (res) {
+						        console.log(res.data) // data
+						    }
+						})
+						wx.hideToast()
+						uni.showModal({
+						    title: '淘口令已经复制',
+							content: '请打开【手机淘宝/天猫】获取活动详情吧！',
+							showCancel: false
+						});
+				    }
 				})
 			}
 			//#endif
